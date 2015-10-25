6d4b3f87a14706caeb6aaa6bde350b01aba83502
noe stauffert <noe.stauffert@gmail.com>
2015-10-25 21:29:17 +0100

    fixed typo in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :contest
   belongs_to :user
-  has_may :votes
+  has_many :votes
 
-  has_reputation :votes, source: :user, aggregated_by: :sum
+  #has_reputation :votes, source: :user, aggregated_by: :sum
 end
